30afc0120af755f1178e94af744f6ab0b9c712bd
sdwhturbosun <49429888+sdwhturbosun@users.noreply.github.com>
2019-11-10 10:58:36 +0800

    Add files via upload

--- adxl345.py
+++ adxl345.py
@@ -99,13 +99,3 @@ class ADXL345:
 
         return {"x": x, "y": y, "z": z}
 
-if __name__ == "__main__":
-    # if run directly we'll just create an instance of the class and output 
-    # the current readings
-    adxl345 = ADXL345()
-    
-    axes = adxl345.getAxes(True)
-    print "ADXL345 on address 0x%x:" % (adxl345.address)
-    print "   x = %.3fG" % ( axes['x'] )
-    print "   y = %.3fG" % ( axes['y'] )
-    print "   z = %.3fG" % ( axes['z'] )
